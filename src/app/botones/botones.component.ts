0a1fd9f5c4203083d734e1758accfb8d9e054192
Carlos Artaza <carlos.artaza73@gmail.com>
2024-03-31 23:16:25 -0300

    Agregado de Type

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -12,12 +12,31 @@ export class BotonesComponent {
   tipo: string = "text";
 
   cambiarTipo(): void {
-    if (this.tipo == "text")
+
+    if (this.tipo == "range") {
+      this.tipo = "text";
+      return;
+    }
+    if (this.tipo == "text") {
       this.tipo = "date";
-      return
-    if (this.tipo == "date")
-      this.tipo = "date";
-      return
+      return;
+    }
+    if (this.tipo == "date") {
+      this.tipo = "checkbox";
+      return;
+    }
+    if (this.tipo == "checkbox") {
+      this.tipo = "color";
+      return;
+    }
+    if (this.tipo == "color") {
+      this.tipo = "file";
+      return;
+    }
+    if (this.tipo == "file") {
+      this.tipo = "range";
+      return;
+    }
 
   }
 }
