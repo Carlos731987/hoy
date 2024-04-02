780dd2893a53e8c514f140164bbfd6c878b70e18
Carlos Artaza <carlos.artaza73@gmail.com>
2024-04-02 20:08:45 -0300

    agregado de directorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Botones2Component } from './componenentes/botones2/botones2.component';
 import { BotonesComponent } from './componenentes/botones/botones.component';
 import { Botones3Component } from './componenentes/botones-3/botones-3.component';
-
+import { ErrorComponent } from './componenentes/error/error.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Botones2Component, BotonesComponent, Botones3Component],
+  imports: [RouterOutlet, Botones2Component, BotonesComponent, Botones3Component, ErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Botones2Component } from './componenentes/botones2/botones2.component';
+import { BotonesComponent } from './componenentes/botones/botones.component';
+import { Botones3Component } from './componenentes/botones-3/botones-3.component';
+import { ErrorComponent } from './componenentes/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+{path:'hola', component: Botones2Component},
+{path:'chau', component: BotonesComponent},
+{path:'pepe', component: Botones3Component},
+{path:'**', component: ErrorComponent}
+
+];
