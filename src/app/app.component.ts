0f6632e92866351d1b9f8ee1eb1107cb28c69ad7
Carlos Artaza <carlos.artaza73@gmail.com>
2024-04-02 18:27:18 -0300

    actualizar directorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Botones2Component } from './botones2/botones2.component';
-import { BotonesComponent } from './botones/botones.component';
-import { Botones3Component } from './botones-3/botones-3.component';
+import { Botones2Component } from './componenentes/botones2/botones2.component';
+import { BotonesComponent } from './componenentes/botones/botones.component';
+import { Botones3Component } from './componenentes/botones-3/botones-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Botones2Component, BotonesComponent, Botones3Component  ],
+  imports: [RouterOutlet, Botones2Component, BotonesComponent, Botones3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
