0c8f20b0afdbd82145f74f178666ebbaa5c3f521
Carlos Artaza <carlos.artaza73@gmail.com>
2024-04-02 18:22:07 -0300

    tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Botones2Component } from './botones2/botones2.component';
 import { BotonesComponent } from './botones/botones.component';
+import { Botones3Component } from './botones-3/botones-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Botones2Component, BotonesComponent],
+  imports: [RouterOutlet, Botones2Component, BotonesComponent, Botones3Component  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/botones-3/botones-3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones-3',
+  standalone: true,
+  imports: [],
+  templateUrl: './botones-3.component.html',
+  styleUrl: './botones-3.component.css'
+})
+export class Botones3Component {
+
+}
